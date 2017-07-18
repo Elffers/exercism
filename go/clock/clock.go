14b7da3930c99690363abaabaa606cc865dd4843
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-07-18 16:44:50 -0700

    Refactor clock to be integer type

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -4,48 +4,19 @@ import "fmt"
 
 const testVersion = 4
 
-type Clock struct{h, m int }
+type Clock int
 
 func New(hour, minute int) Clock {
-	var m, overflow int
-	overflow, m = divmod(minute, 60)
-
-	h := mod(hour + overflow, 24)
-
-	if minute < 0 {
-		h = mod(h-1, 24)
-	}
-
-	return Clock{h, m}
+	minutes := mod((hour*60+minute), 1440)
+	return Clock(minutes)
 }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.h, c.m)
+	return fmt.Sprintf("%02d:%02d", c /60, c % 60)
 }
 
 func (c Clock) Add(minutes int) Clock {
-	raw_minutes := c.m + minutes
-
-	var m, overflow int
-	overflow, m = divmod(raw_minutes, 60)
-
-	h := mod(c.h + overflow, 24)
-
-	if raw_minutes < 0 && m != 0 { // this is mysterious
-		h = mod(h-1, 24)
-	}
-
-	c.m = m
-	c.h = h
-
-	return c
-}
-
-func abs(i int) int {
-	if i < 0 {
-		return -i
-	}
-	return i
+	return New(0, (int(c)+ minutes) % 1440)
 }
 
 func mod(n int, d int) int {
@@ -54,11 +25,4 @@ func mod(n int, d int) int {
 		r += d
 	}
 	return r
-}
-
-func divmod(n int, d int) (int, int) {
-	q := n / d
-	r := mod(n, d)
-
-	return q, r
 }
