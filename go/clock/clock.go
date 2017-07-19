e257faceff0f1a73bdbae3b4a775739c3c81bfd3
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-07-18 17:00:39 -0700

    go fmt clock

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -7,16 +7,16 @@ const testVersion = 4
 type Clock int
 
 func New(hour, minute int) Clock {
-	minutes := mod((hour*60+minute), 1440)
+	minutes := mod((hour*60 + minute), 1440)
 	return Clock(minutes)
 }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c /60, c % 60)
+	return fmt.Sprintf("%02d:%02d", c/60, c%60)
 }
 
 func (c Clock) Add(minutes int) Clock {
-	return New(0, int(c)+ minutes)
+	return New(0, int(c)+minutes)
 }
 
 func mod(n int, d int) int {
