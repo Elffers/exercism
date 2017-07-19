4f38dd662308f1bcbf3cee599e6713ed9ee08dde
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-07-19 11:29:10 -0700

    Better naming

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -2,11 +2,12 @@ package accumulate
 
 const testVersion = 1
 
-func Accumulate(list []string, f func(string) string) []string {
-	col := make([]string, len(list))
+func Accumulate(in []string, f func(string) string) []string {
+	out := make([]string, len(in))
 
-	for i, str := range list {
-		col[i] = f(str)
+	for i, val := range in {
+		out[i] = f(val)
 	}
-	return col
+
+	return out
 }
