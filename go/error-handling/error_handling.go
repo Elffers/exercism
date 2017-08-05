b6f828562e5900e77d7bde4831a71330313b1934
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-08-05 13:48:25 -0700

    Add error_handling dir

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -3,6 +3,20 @@ package erratum
 const testVersion = 2
 
 func Use(o ResourceOpener, input string) (err error) {
+	// Alternative approach:
+	// var rsc Resource
+	// for {
+	// 	rsc, err = o()
+	// 	if err == nil {
+	// 		break
+	// 	}
+
+	// 	if _, ok := err.(TransientError); ok {
+	// 		continue
+	// 	}
+	// 	return err
+	// }
+
 	res, err := o()
 
 	if err != nil {
@@ -17,7 +31,7 @@ func Use(o ResourceOpener, input string) (err error) {
 		if p := recover(); p != nil {
 			if frob, ok := p.(FrobError); ok {
 				res.Defrob(frob.defrobTag)
-				err = frob.inner
+				// err = frob.inner
 			}
 			err = p.(error)
 		}
