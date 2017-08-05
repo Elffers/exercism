50251aede140c498226dd5d2fdd7650b8e352612
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-08-04 20:24:23 -0700

    Error handling: TestKeepTryOpenOnTransient passes

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -2,11 +2,17 @@ package erratum
 
 const testVersion = 2
 
-func Use(o ResourceOpener, input string) (err error) {
-	res, _ := o()
+func Use(o ResourceOpener, input string) error {
+	res, err := o()
+
+	if err != nil {
+		if _, ok := err.(TransientError); ok {
+			return Use(o, input)
+		}
+	}
 
 	res.Close()
 	res.Frob(input)
 
-	return
+	return nil
 }
