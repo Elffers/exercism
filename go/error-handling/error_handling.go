895d3df2eb0eaa9913db18be19701fca7eac68a5
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-08-04 21:12:07 -0700

    Error handling: TestCallDefrobAndCloseOnFrobError passes

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -1,19 +1,32 @@
 package erratum
 
+import "fmt"
+
 const testVersion = 2
 
-func Use(o ResourceOpener, input string) error {
+func Use(o ResourceOpener, input string) (err error) {
 	res, err := o()
 
 	if err != nil {
 		if _, ok := err.(TransientError); ok {
 			return Use(o, input)
 		}
-		return err
+		return
+
 	}
 
-	res.Close()
+	defer func() {
+		if p := recover(); p != nil {
+			if frob, ok := p.(FrobError); ok {
+				fmt.Println("FROB")
+				res.Defrob(frob.defrobTag)
+				err = frob.inner
+			}
+		}
+		res.Close()
+	}()
+
 	res.Frob(input)
 
-	return nil
+	return err
 }
