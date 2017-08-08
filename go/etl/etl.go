caed115fc74efdb2afdebfd2db9f7db266722c9c
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-08-08 13:55:09 -0700

    Refactor ETL

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -6,16 +6,11 @@ const testVersion = 1
 
 func Transform(input map[int][]string) map[string]int {
 	output := make(map[string]int)
-	for k, v := range input {
-		for _, letter := range v {
-			newKey := strings.ToLower(letter)
-			_, ok := output[newKey]
-
-			if !ok {
-				output[newKey] = k
-			}
+	for score, letters := range input {
+		for _, letter := range letters {
+			key := strings.ToLower(letter)
+			output[key] = score
 		}
 	}
 	return output
-
 }
