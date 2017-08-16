b6fa2d62e352829e715dd47f9a4d827484fcabac
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-08-16 10:34:57 -0700

    Make grains faster (go)
    using bit shifting instead of math

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -2,7 +2,6 @@ package grains
 
 import (
 	"fmt"
-	"math"
 )
 
 const testVersion = 1
@@ -11,7 +10,7 @@ func Square(s int) (uint64, error) {
 	if s < 1 || s > 64 {
 		return 1, fmt.Errorf("foo")
 	}
-	grains := math.Pow(2, float64(s-1))
+	grains := 1 << uint(s-1)
 	return uint64(grains), nil
 }
 
