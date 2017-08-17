a98d26a640529233420a670b7984b49b0fea2d91
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-08-17 13:32:49 -0700

    Refactor isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -8,12 +8,12 @@ func IsIsogram(s string) bool {
 	seen := make(map[rune]bool)
 	for _, r := range s {
 		key := unicode.ToLower(r)
-		if _, ok := seen[key]; ok {
+
+		if unicode.IsLetter(r) && seen[key] {
 			return false
 		}
-		if unicode.IsLetter(r) {
-			seen[key] = true
-		}
+
+		seen[key] = true
 	}
 	return true
 }
