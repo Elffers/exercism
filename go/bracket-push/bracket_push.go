dc5060435007ca0bf14c8c96d82d6a144929fa5d
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-09-06 10:00:32 -0700

    Solved bracket push (go)

---
+++ go/bracket-push/bracket_push.go
@@ -0,0 +1,47 @@
+package brackets
+
+const testVersion = 5
+
+var match = map[rune]rune{
+	']': '[',
+	'}': '{',
+	'"': '"',
+	')': '(',
+}
+
+func pop(stack []rune) (rune, []rune) {
+	return stack[len(stack)-1], stack[:len(stack)-1]
+}
+
+func Bracket(in string) (bool, error) {
+	if len(in) == 0 {
+		return true, nil
+	}
+
+	stack := []rune{}
+
+	for _, r := range in {
+		if r == '[' || r == '(' || r == '{' || r == '"' {
+			stack = append(stack, r)
+		} else {
+			if r == ']' || r == ')' || r == '}' || r == '"' {
+				if len(stack) == 0 {
+					return false, nil
+				}
+
+				el, newStack := pop(stack)
+				stack = newStack
+
+				if el != match[r] {
+					return false, nil
+				}
+			}
+		}
+	}
+
+	if len(stack) > 0 {
+		return false, nil
+	}
+
+	return true, nil
+}
