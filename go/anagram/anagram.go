1f9d48e64602755b96a0b9a924d3f9f6a0369d35
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-09-07 10:58:58 -0700

    Refactor anagram

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,18 +1,15 @@
 package anagram
 
 import (
-	"sort"
 	"strings"
 )
 
 const testVersion = 2
 
-type sortRunes []rune
-
 func Detect(word string, candidates []string) []string {
 	word = strings.ToLower(word)
-
 	sublist := []string{}
+
 	for _, c := range candidates {
 		if isAnagram(word, strings.ToLower(c)) {
 			sublist = append(sublist, c)
@@ -22,26 +19,14 @@ func Detect(word string, candidates []string) []string {
 }
 
 func isAnagram(a, b string) bool {
-	if a == b {
+	if a == b || len(a) != len(b) {
 		return false
 	}
-	return SortString(a) == SortString(b)
-}
 
-func (s sortRunes) Less(i, j int) bool {
-	return s[i] < s[j]
-}
-
-func (s sortRunes) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s sortRunes) Len() int {
-	return len(s)
-}
-
-func SortString(s string) string {
-	r := []rune(s)
-	sort.Sort(sortRunes(r))
-	return string(r)
+	for _, r := range a {
+		if strings.Count(a, string(r)) != strings.Count(b, string(r)) {
+			return false
+		}
+	}
+	return true
 }
