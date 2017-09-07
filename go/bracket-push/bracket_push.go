7fbde31e6cc2dac051406a7b67ec0ad67089fd21
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-09-06 17:09:46 -0700

    Refactor bracket push
    because strings are already arrays

--- go/bracket-push/bracket_push.go
+++ go/bracket-push/bracket_push.go
@@ -5,39 +5,29 @@ const testVersion = 5
 var match = map[rune]rune{
 	']': '[',
 	'}': '{',
-	'"': '"',
 	')': '(',
 }
 
-func pop(stack []rune) (rune, []rune) {
-	return stack[len(stack)-1], stack[:len(stack)-1]
-}
-
 func Bracket(in string) (bool, error) {
-	stack := []rune{}
-
+	stack := ""
 	for _, r := range in {
-		if r == '[' || r == '(' || r == '{' || r == '"' {
-			stack = append(stack, r)
-		} else {
-			if r == ']' || r == ')' || r == '}' || r == '"' {
-				if len(stack) == 0 {
-					return false, nil
-				}
+		if r == '[' || r == '(' || r == '{' {
+			stack += string(r)
+		}
 
-				el, newStack := pop(stack)
-				stack = newStack
+		if r == ']' || r == ')' || r == '}' {
+			if len(stack) == 0 {
+				return false, nil
+			}
 
-				if el != match[r] {
-					return false, nil
-				}
+			popped := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			if rune(popped) != match[r] {
+				return false, nil
 			}
 		}
 	}
 
-	if len(stack) > 0 {
-		return false, nil
-	}
-
-	return true, nil
+	return len(stack) == 0, nil
 }
