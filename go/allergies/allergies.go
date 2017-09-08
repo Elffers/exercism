78f3c0617a09b8802c1ad96eaf005db5007911fa
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-09-08 10:34:59 -0700

    Refactor allergies

--- go/allergies/allergies.go
+++ go/allergies/allergies.go
@@ -2,41 +2,27 @@ package allergies
 
 const testVersion = 1
 
-type Allergen int
-
-const (
-	Eggs Allergen = 1 << iota
-	Peanuts
-	Shellfish
-	Strawberries
-	Tomatoes
-	Chocolate
-	Pollen
-	Cats
-)
-
-var allergens = map[string]Allergen{
-	"eggs":         Eggs,
-	"peanuts":      Peanuts,
-	"shellfish":    Shellfish,
-	"strawberries": Strawberries,
-	"tomatoes":     Tomatoes,
-	"chocolate":    Chocolate,
-	"pollen":       Pollen,
-	"cats":         Cats,
+var allergens = map[string]uint{
+	"eggs":         1,
+	"peanuts":      2,
+	"shellfish":    4,
+	"strawberries": 8,
+	"tomatoes":     16,
+	"chocolate":    32,
+	"pollen":       64,
+	"cats":         128,
 }
 
 func AllergicTo(score uint, a string) bool {
 	allergen := allergens[a]
-	return score&uint(allergen) != 0
+	return score&allergen != 0
 }
 
 func Allergies(score uint) []string {
 	allergies := []string{}
-	for k, v := range allergens {
-		bit := score & uint(v)
-		if bit != 0 {
-			allergies = append(allergies, k)
+	for allergen, s := range allergens {
+		if score&s != 0 {
+			allergies = append(allergies, allergen)
 		}
 	}
 	return allergies
