d49ac76f3b3a993b163e0cf4dc7aa753385b7e53
Hsing-Hui Hsu <hsinghui@amazon.com>
2017-10-10 15:15:16 -0700

    Different approach to RNA transcription

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,20 +1,15 @@
 package strand
 
+import "strings"
+
 const testVersion = 3
 
 func ToRNA(dna string) string {
-	rna := ""
-	for _, r := range dna {
-		switch r {
-		case 'C':
-			rna += "G"
-		case 'G':
-			rna += "C"
-		case 'A':
-			rna += "U"
-		case 'T':
-			rna += "A"
-		}
-	}
-	return rna
+	r := strings.NewReplacer(
+		"C", "G",
+		"G", "C",
+		"A", "U",
+		"T", "A",
+	)
+	return r.Replace(dna)
 }
